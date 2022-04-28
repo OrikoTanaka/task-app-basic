a2db7f7b4e94515753e421050c564294c4f71acb
OrikoTanaka <oriko644@gmail.com>
2022-04-28 08:34:51 +0000

    tasksテーブルにnameカラム追加 4/28

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220428072510) do
+ActiveRecord::Schema.define(version: 20220428083240) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "note"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
