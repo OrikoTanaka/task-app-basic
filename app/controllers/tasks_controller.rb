d92afff05ef2c0ad1541389b8833602806ec37b2
OrikoTanaka <oriko644@gmail.com>
2022-05-09 03:55:00 +0000

    5/9タスク一覧表示とりあえずOK

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,11 @@
 class TasksController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @tasks = @user.tasks
+  end
+  
+  def new
+    @user = User.find(params[:user_id])
+  end
 end
+
