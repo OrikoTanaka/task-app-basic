41c398cd4c028b27128b2358384955d2034267cb
OrikoTanaka <oriko644@gmail.com>
2022-05-15 08:46:10 +0000

    C8 タスク詳細ページ作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,41 @@ class TasksController < ApplicationController
     @tasks = @user.tasks
   end
   
+  def show
+    @user = User.find(params[:user_id])
+    @task = Task.find(params[:id])
+  end
+  
   def new
     @user = User.find(params[:user_id])
+    @task = Task.new
   end
+  
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url(current_user)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    redirect_to user_task_url(current_user)
+  end
+  
+  
+  private
+  
+    def task_params
+      params.require(:task).permit(:name, :note)
+    end
 end
 
